f1213d0e021b2faa67ccbd66260bbc0c1fff3a9c
Modestas <modestaszk@gmail.com>
2019-04-29 21:21:37 +0300

    bandtmai javascripto

---
+++ bandymai/index.js
@@ -0,0 +1,19 @@
+let number1 = 25;
+let number2 = 48;
+let number3 = 42;
+
+let string1 = `JavaScript is a multi-paradigm,
+dynamic language with types and operators, standard built-in objects, and methods.
+ Its syntax is based on the Java
+  and C languages — many structures from those languages apply
+   to JavaScript as well.`
+   let stringas2 = `The ultimate chocolate chip cookie cake is a gigantic
+   chocolate chip cookie baked into a cake a pan and decorated with frosting.`
+
+
+   console.log(string1.length());
+   console.log(string1.split( / / ));
+   console.log(string1.toUpperCase());
+   console.log(string1.repeat(8));
+   console.log(stringas2.replace(`chocolate`, `sugar`));
+   console.log(number1.toString());
