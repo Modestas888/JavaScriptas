07ce7ca6f797f22fdf1fe7d293c3d835e9e07e27
Modestas <modestaszk@gmail.com>
2019-05-07 17:52:37 +0300

    funkciju pavadinimu taisymas

--- Guests list/index.js
+++ Guests list/index.js
@@ -8,63 +8,60 @@ function myFunction() {
 
 }
 
-document.getElementById("addstart").addEventListener("click", Function);
-function Function() {
+document.getElementById("addstart").addEventListener("click", unshiftinimas);
+function unshiftinimas() {
   pushing.unshift(document.getElementById("input").value);
   document.getElementById("demo").innerHTML = pushing;
   // add start
 }
-document.getElementById("removefirst").addEventListener("click", mFunction);
-function mFunction() {
+document.getElementById("removefirst").addEventListener("click", removinimasFirst);
+function removinimasFirst() {
     pushing.shift().value;
   document.getElementById("demo").innerHTML = pushing;
   // remove first
 }
 //
-  document.getElementById("removelast").addEventListener("click", yFunction);
-function yFunction() {
+  document.getElementById("removelast").addEventListener("click", removinimaslast);
+function removinimaslast() {
   pushing.pop().value;
   document.getElementById("demo").innerHTML = pushing;
 }
 // // remove last
 //
-document.getElementById("reverselist").addEventListener("click", myFunctio);
-function myFunctio() {
+document.getElementById("reverselist").addEventListener("click", reversinimas);
+function reversinimas() {
   pushing.reverse();
   document.getElementById("demo").innerHTML = pushing;
 }
 // // reverse
-document.getElementById("splices").addEventListener("click", myFunctio);
-function myFunctio() {
+document.getElementById("splices").addEventListener("click", splicinimas);
+function splicinimas() {
   pushing.splice(1,2);
   document.getElementById("demo").innerHTML = pushing;
 }
 // // splice
-document.getElementById("golast").addEventListener("click", myFunctio);
-function myFunctio() {
+document.getElementById("golast").addEventListener("click", golastinimas);
+function golastinimas() {
   pushing.push(pushing.shift());
   document.getElementById("demo").innerHTML = pushing;
 }
 // // go last
-document.getElementById("gofirst").addEventListener("click", myFunctio);
-function myFunctio() {
+document.getElementById("gofirst").addEventListener("click", gofirstinimas);
+function gofirstinimas() {
   pushing.unshift(pushing.pop());
   document.getElementById("demo").innerHTML = pushing;
 }
 // go first
-document.getElementById("gofirst").addEventListener("click", myFunctio);
-function myFunctio() {
-  pushing.unshift(pushing.pop());
-  document.getElementById("demo").innerHTML = pushing;
-}
-document.getElementById("sortaz").addEventListener("click", myFunctios);
-function myFunctios() {
+
+document.getElementById("sortaz").addEventListener("click", sortinimasaz);
+function sortinimasaz() {
   pushing.sort((a, b) => a.localeCompare(b));
   document.getElementById("demo").innerHTML = pushing;
 }
-
-document.getElementById("sortza").addEventListener("click", sortas);
-function sortas() {
+// sortinimas az
+document.getElementById("sortza").addEventListener("click", sortasza);
+function sortasza() {
   pushing.sort((b, a) => a.localeCompare(b));
   document.getElementById("demo").innerHTML = pushing;
 }
+// sortinimas za
